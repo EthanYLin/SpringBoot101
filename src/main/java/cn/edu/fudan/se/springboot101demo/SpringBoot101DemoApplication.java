8a48b369e8848f9f732577645a14607ae4ca4870
Lin Yicheng <yichenglin20@fudan.edu.cn>
2025-02-27 16:41:04 +0800

    initial commit

---
+++ src/main/java/cn/edu/fudan/se/springboot101demo/SpringBoot101DemoApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.fudan.se.springboot101demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot101DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot101DemoApplication.class, args);
+    }
+
+}
